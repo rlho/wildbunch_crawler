6e9b96e40f7cde824b4863ea124e9cc4f255a848
Riho <riho.takagi@vasily.jp>
2018-06-24 10:35:00 +0900

    fix crawler

--- wildbunch_crawler.rb
+++ wildbunch_crawler.rb
@@ -1,37 +1,55 @@
 #!/usr/bin/ruby
 
-# About nokogiri
-
 require 'open-uri'
 require 'nokogiri'
 require 'line_notify'
 
 class WildBunchCrawler
-  LINE_TOKEN = 'nwyZvycBGboIloWRGTp6H8FYad7p0abpUtM9KjJgegm'
   def execute
-    line_notify = LineNotify.new(LINE_TOKEN)
+    comfort = Parse.new(site_name: 'コンフォートホテル', url: "https://asp.hotel-story.ne.jp/ver3d/planlist.asp?hcod1=CH410&hcod2=001&LB01=server4&mode=seek&hidSELECTARRYMD=2018/7/28&hidSELECTHAKSU=1&sortmethod=priceup&clrmode=seek", xpath: '//div[@class="plan-list"]')
+    comfort.check_stock
 
-    comfort_doc = Nokogiri.HTML(open("https://asp.hotel-story.ne.jp/ver3d/planlist.asp?hcod1=CH410&hcod2=001&LB01=server4&mode=seek&hidSELECTARRYMD=2018/7/28&hidSELECTHAKSU=1&sortmethod=priceup&clrmode=seek"))
-    comport_text = comfort_doc.xpath('//div[@class="plan-list"]').text
+    toyoko_in = Parse.new(site_name: '東横イン', url: 'https://www.toyoko-inn.com/search/result?chck_in=2018/07/28&inn_date=1&rsrv_num=1&sel_ldgngPpl=2&sel_area=42&sel_area_txt=%E5%B1%B1%E5%8F%A3&sel_htl=&rd_smk=&sel_room_clss_Id=&sel_prkng=&sel_cnfrnc=&sel_hrtfll_room=&sel_whlchr=&sel_bath=&sel_rstrnt=&srch_key_word=&lttd=&lngtd=&pgn=1&sel_dtl_cndtn=on&prcssng_dvsn=dtl&', xpath: '//div[@class="listHead mt20"]/p[@class="txtMessage"]')
+    toyoko_in.check_stock
 
-    options = { message: "コンフォートホテル: #{stock_text(comport_text)}" }
-    line_notify.send(options)
-
-    toyoko_doc = Nokogiri.HTML(open("https://www.toyoko-inn.com/search/result?chck_in=2018/07/28&inn_date=1&rsrv_num=1&sel_ldgngPpl=2&sel_area=42&sel_area_txt=%E5%B1%B1%E5%8F%A3&sel_htl=&rd_smk=&sel_room_clss_Id=&sel_prkng=&sel_cnfrnc=&sel_hrtfll_room=&sel_whlchr=&sel_bath=&sel_rstrnt=&srch_key_word=&lttd=&lngtd=&pgn=1&sel_dtl_cndtn=on&prcssng_dvsn=dtl&"))
-    toyoko_text = toyoko_doc.xpath('//div[@class="listHead mt20"]/p[@class="txtMessage"]').text
-
-    options = { message: "東横イン: #{stock_text(toyoko_text)}" }
-    line_notify.send(options)
-
-    active_doc = Nokogiri.HTML(open("https://ishidaya.rwiths.net/r-withs/tfs0020a.do?hotelNo=15761&vipCode=&sort=1&curPage=1&f_lang=ja&ciDateY=2018&ciDateM=07&ciDateD=28&coDateY=2018&coDateM=07&coDateD=29&otona=2&s1=0&s2=0&y1=0&y2=0&y3=0&y4=0&room=1"))
-    active_text = active_doc.xpath('//div[@class="planList"]').text
-
-    options = { message: "アクティブ: #{stock_text(active_text)}" }
-    line_notify.send(options)
-  end
-
-  def stock_text(text)
-    text.nil? || text.empty? ? 'なし' : 'あり'
+    active = Parse.new(site_name: 'アクティブ', url: 'https://ishidaya.rwiths.net/r-withs/tfs0020a.do?hotelNo=15761&vipCode=&sort=1&curPage=1&f_lang=ja&ciDateY=2018&ciDateM=07&ciDateD=28&coDateY=2018&coDateM=07&coDateD=29&otona=2&s1=0&s2=0&y1=0&y2=0&y3=0&y4=0&room=1', xpath: '//div[@class="planList"]')
+    active.check_stock
   end
 end
+
+class Parse
+
+  LINE_TOKEN = 'nwyZvycBGboIloWRGTp6H8FYad7p0abpUtM9KjJgegm'
+
+  def initialize(site_name:, url:, xpath:)
+    @site_name = site_name
+    @url = url
+    @xpath = xpath
+  end
+
+  def check_stock
+    p @site_name
+    p stock
+    send_line if has_stock?
+  end
+
+  def doc
+    Nokogiri.HTML(open(@url))
+  end
+
+  def stock
+    doc.xpath(@xpath)
+  end
+
+  def has_stock?
+    !(stock.nil? || stock.empty?)
+  end
+
+  def send_line
+    line_notify = LineNotify.new(LINE_TOKEN)
+    options = { message: "#{@site_name}: 空室があります!!!!!!" }
+    line_notify.send(options)
+  end
+end
+
 WildBunchCrawler.new.execute
