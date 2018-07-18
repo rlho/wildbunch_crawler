73c4bb869c3cdb394b5b141cb54bbf22427bcefc
Riho <riho.takagi@vasily.jp>
2018-07-19 00:08:51 +0900

    fix

--- wildbunch_crawler.rb
+++ wildbunch_crawler.rb
@@ -9,7 +9,7 @@ class WildBunchCrawler
     comfort = Parse.new(site_name: 'コンフォートホテル', url: "https://asp.hotel-story.ne.jp/ver3d/planlist.asp?hcod1=CH410&hcod2=001&LB01=server4&mode=seek&hidSELECTARRYMD=2018/7/29&hidSELECTHAKSU=1&sortmethod=priceup&clrmode=seek", xpath: '//div[@class="plan-list"]')
     comfort.check_stock
 
-    toyoko_in = Parse.new(site_name: '東横イン', url: 'https://www.toyoko-inn.com/search/result?chck_in=2018/07/29&inn_date=1&rsrv_num=1&sel_ldgngPpl=2&sel_area=42&sel_area_txt=%E5%B1%B1%E5%8F%A3&sel_htl=&rd_smk=&sel_room_clss_Id=&sel_prkng=&sel_cnfrnc=&sel_hrtfll_room=&sel_whlchr=&sel_bath=&sel_rstrnt=&srch_key_word=&lttd=&lngtd=&pgn=1&sel_dtl_cndtn=on&prcssng_dvsn=dtl&', xpath: '//div[@class="listHead mt20"]/p[@class="txtMessage"]')
+    toyoko_in = Parse.new(site_name: '東横イン', url: 'https://www.toyoko-inn.com/search/result?chck_in=2018/07/29&inn_date=1&rsrv_num=1&sel_ldgngPpl=2&sel_area=42&sel_area_txt=%E5%B1%B1%E5%8F%A3&sel_htl=&rd_smk=&sel_room_clss_Id=&sel_prkng=1&sel_prkng=1&pln_only=&rsrv_only=&sel_cnfrnc=1&sel_hrtfll_room=1&sel_whlchr=&sel_bath=&sel_rstrnt=&srch_key_word=&lttd=&lngtd=&pgn=1&sel_dtl_cndtn=on&prcssng_dvsn=dtl&', xpath: '//div[@class="listHead mt20"]/p[@class="txtMessage"]')
     toyoko_in.check_stock
 
     active = Parse.new(site_name: 'アクティブ', url: 'https://ishidaya.rwiths.net/r-withs/tfs0020a.do?hotelNo=15761&GCode=ishidaya&vipCode=&sort=1&curPage=1&f_lang=ja&ciDateY=2018&ciDateM=07&ciDateD=29&lowerCharge=0&upperCharge=999999&coDateY=2018&coDateM=07&coDateD=30&otona=2&s1=0&s2=0&y1=0&y2=0&y3=0&y4=0&room=1', xpath: '//div[@class="planList"]')
